58807ebc48526928853e492169046f6983ec06be
andrea351 <giuliotti.2107814@studenti.uniroma1.it>
2025-03-02 18:16:38 +0000

    Modificato

--- T01/E1-uint2bin/e1.c
+++ T01/E1-uint2bin/e1.c
@@ -1,2 +1,16 @@
 // Inserire la soluzione qui...
-
+#include <stdio.h>
+#include "e1.h"
+void uint2bin(unsigned x, char bin[32]){
+    for (int i = 1; i <= 32; i++){
+        if (x & 1){ // se (x & 1) == 1 significa che l'ultimo bit di x è 1, quindi metto
+            // '1' nell'array bin. dopodichè avanzo con x >> 1 e ricontrollo. 
+            // ad esempio, un x iniziale del tipo 0011, dopo l aprima iterazione sarà 0001 ec...
+            // nel frattempo i numeri scartati vengono confrontati con & e il ris inserito in bin
+            // in posizione ultima, penultima ecc...
+            bin[32 - i] = '1';
+        }
+        else bin[32 - i] = '0';
+        x = x >> 1;
+    }
+}
--- T01/E3/e3.c
+++ T01/E3/e3.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct node {
     char * id;
     char * value;
     struct node * next;
-} node_t;,
+} node_t;
 
 static int count = 0;
 
 node_t* add_node(node_t* l, char* value) {
     node_t* node = malloc(sizeof(node_t));
 
-    char id[16];
+    /*char id[16];
     sprintf(id, "ID_%d", count++);
     node->id = id;
     node->value = value;
@@ -20,11 +21,20 @@ node_t* add_node(node_t* l, char* value) {
     if (l != NULL)
         node->next = l;
    
-    return node;
+    return node;*/
+    node->id = malloc(16 * sizeof(char));
+    sprintf(node->id, "ID_%d", count++);
+
+    node->value = malloc((strlen(value) + 1) * sizeof(char));
+    strcpy(node->value, value);
+
+    if (l != NULL) node->next = l;
+    else return node;
+
 }
 
 void print_list(node_t* head) {
-    node_t* current = head;
+    node_t* current = NULL;
 
     while (current != NULL) {
         printf("%s\n", current->value);
@@ -33,12 +43,23 @@ void print_list(node_t* head) {
 }
 
 void delete_list(node_t * head) {
-    node_t* current = head;
+    node_t* current = NULL;
+    if (head != NULL) {
+        node_t* current = head;
+    }
+    node_t* next = NULL;
 
     while (current != NULL) {
-        free(current);
+        /*node_t* temp = current;
+        current = current->next;*/
+        /*free(current);
+        free(current->value);*/
         free(current->value);
-        current = current->next;
+        free(current->id);
+        next = current->next;
+        free(current);
+        current = next;
+        // current = current->next;
     }
 }
 
